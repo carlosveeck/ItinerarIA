5ce6b6c4522813efb7838308fab43deeb5c96e52
Rodrigo Gomes <159979512+rRodrigoGomes@users.noreply.github.com>
2024-12-02 09:36:44 -0300

    att formato de requisição e resposta

--- schemas.py
+++ schemas.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
 
 class promptrequest(BaseModel):
-    mensagem : str
-    parametros : dict
+    model : str
+    messages : list
+    max_tokens : int
+    temperature : float
 
 class promptresponse(BaseModel):
-    resposta : str
-    #ajudar no controle de quantas keys estao sendo usadas
-    keys_usadas : dict
+    message : dict
+    #para controle de tokens usados
+    usage : dict
 
