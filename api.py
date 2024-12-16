1dd19f3eb53326cb7a5e994a45ed57e79cbf78a8
caionsantos <137624365+caionsantos@users.noreply.github.com>
2024-12-16 07:47:33 -0300

    feat: criação de um ambiente virtual

---
+++ api.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+class Request(BaseModel):
+    user_input: str
+
+@app.get("/")
+async def ok_endpoint():
+    return {"message":"ok"}
+
+@app.get("/hello")
+async def hello_endpoint(name: str = 'World'):
+    return {"message": f"Hello, {name}!"}
+
+@app.post("/input")
+async def place_input(user_input: str):
+    return{"message": f"user input posted successfully: {user_input}"}
+
+@app.post("/input_pydantic")
+async def place_input(user: Request):
+    return{"message": f"user input posted successfully: {user.user_input}"}
